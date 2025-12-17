f7b864d79290cba1c0a5a4d55c198a74c58fc8f3
Lucie PEYROT <lucie.peyrot@student-cs.fr>
2025-12-17 16:21:12 +0100

    [Obs] using json values

--- Observabilité/observability_theta1_in_y.jl
+++ Observabilité/observability_theta1_in_y.jl
@@ -3,6 +3,8 @@ module Observability
 using LinearAlgebra
 using Symbolics
 using ModelingToolkit
+using JSON
+
 
 include("../Model/model_theta1_in_Y.jl")
 using .ModelTheta1InY
@@ -26,65 +28,13 @@ OO = vcat(OO...)  # concatenate vertically
 @show size(C)
 @show OO
 
-@parameters t
-@parameters J1 J2 D1 D2 KL Ks ω0 
-@parameters α1 α2 β1 β2 Pr1 Pr2 P0 PG1 PG2 θ1
-@variables ω1(t) Tm1(t)
-@variables θ2(t) ω2(t) Tm2(t)   
-@variables N(t)
+@parameters J1 J2 D1 D2 KL Ks ω0 ω1 ω2 Tm1 Tm2 N
+@parameters α1 α2 β1 β2 Pr1 Pr2 P0 PG1 PG2 θ1 
+
 
 # dictionnaire de valeurs numériques
-valeurs_numeriques = Dict(
-    # --- 1. Paramètres du Réseau & Globaux ---
-    KL => 3064.0,       # Raideur pour 2 lignes (MW/rad) [cite: 113]
-    Ks => 0.05,         # Gain contrôle secondaire [cite: 99]
-    ω0 => 100 * π,      # Fréquence nominale 50Hz (314.159...) [cite: 97]
-
-    # --- 2. Paramètres Générateur 1 (Table 1)  ---
-    J1 => 0.4,          # Inertie (Note: valeur très faible, possiblement normalisée)
-    D1 => 0.04,         # Amortissement
-    Pr1 => 100.0,       # Puissance de régulation (Pr)
-    α1  => 100.0,        # Gain alpha (noté "C" dans le tableau)
-    β1 => 2000.0,       # Gain beta
-
-    # --- 3. Paramètres Générateur 2 (Table 1)  ---
-    J2 => 0.1,          # Inertie
-    D2 => 0.02,         # Amortissement
-    Pr2 => 50.0,        # Puissance de régulation (Pr)
-    α2 => 100.0,        # Gain alpha
-    β2 => 2000.0,       # Gain beta
-
-    # --- 4. Point de consigne de Puissance (Attention !) ---
-    # Le document a deux consignes distinctes.
-    # Ton code utilisait 'P0'. Idéalement, remplace P0 par P1_0 et P2_0 dans tes équations.
-    # Je les définis ici :
-    P0 => 0.0,          # Valeur par défaut si ton code utilise P0 additivement
-    # Si tu modifies ton modèle, utilise ces valeurs pour les consignes :
-    # P1_0 => 600.0, 
-    # P2_0 => 400.0,
-
-    # --- 5. États Initiaux (Point de Fonctionnement Stable) ---
-    
-    # Vitesses : Nominales
-    ω1 => 100 * π, 
-    ω2 => 100 * π,
-    
-    # Angles : Calculés par l'écoulement de puissance (Load Flow)
-    # P_inj1 = P_G1 (600) - P_L1 (400) = +200 MW [cite: 102, 110]
-    # F12 = 200 MW. Donc 200 = KL * (θ1 - θ2) => Δθ = 200 / 3064
-    θ1 => 200.0 / 3064.0, # approx 0.065 rad
-    θ2 => 0.0,            # Angle de référence
-
-    # Couples Mécaniques (Tm)
-    # À l'équilibre : Tm = P_Gen / ω0 [cite: 37, 40]
-    # P_Gen1 = 600, P_Gen2 = 400
-    Tm1 => 600.0 / (100 * π),  # approx 1.91
-    Tm2 => 400.0 / (100 * π),  # approx 1.27
-
-    # Variable N (Contrôle secondaire)
-    # À l'état stable nominal, N = 0 [cite: 58]
-    N => 0.0
-)
+values_json = JSON.parsefile("Model/numerical_values.json")
+valeurs_numeriques = Dict(Symbol(k)=>v for (k,v) in values_json)
 
 println("--- Substitution Numérique ---")
 
@@ -94,6 +44,5 @@ A_num = Symbolics.value.(substitute(A, valeurs_numeriques))
 C_num = Symbolics.value.(substitute(C, valeurs_numeriques))
 OO_num = Symbolics.value.(substitute(OO, valeurs_numeriques))
 
-@show rank(OO_num)
 
 end # module
